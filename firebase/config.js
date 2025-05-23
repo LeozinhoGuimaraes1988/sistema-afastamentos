9949c94aed9d80990b1c853cbace378611ab05b2
Leonardo Guimarães <94874711+LeozinhoGuimaraes1988@users.noreply.github.com>
2025-05-23 16:09:00 -0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,15 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCnYLc1JzcqA6NYEvgBpQh0g-9N7mbyhcE',
-  authDomain: 'gestaoafastamentos.firebaseapp.com',
-  projectId: 'gestaoafastamentos',
-  storageBucket: 'gestaoafastamentos.appspot.com',
-  messagingSenderId: '659167029343',
-  appId: '1:659167029343:web:6cac0a85279d270edd13ea',
-  measurementId: 'G-XST36V402C',
-};
+
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
